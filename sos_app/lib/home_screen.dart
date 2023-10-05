397fff7cf8aa737b74e5fa65b01e4cb1c4c73d0f
kartikbende <80601764+kartikbende@users.noreply.github.com>
2023-10-05 19:45:27 +0530

    5
    added the random quotes feature

--- sos_app/lib/home_screen.dart
+++ sos_app/lib/home_screen.dart
@@ -1,14 +1,31 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:sos_app/widgets/custom_app_bar.dart';
 
+// ignore: must_be_immutable
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  //const HomeScreen({super.key});
+  int qIndex = 0;
+
+  getRandomQuote() {
+    Random random = Random();
+    qIndex = random.nextInt(6);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(children: [custom_app_bar()]),
-    ));
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    custom_app_bar(
+                      quoteeIndex: qIndex,
+                      onTap: getRandomQuote(),
+                    ),
+                  ],
+                ))));
   }
 }

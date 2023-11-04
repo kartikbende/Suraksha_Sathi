84565f183a7a80cabb91aabe854783c5423d1e19
kartikbende <80601764+kartikbende@users.noreply.github.com>
2023-11-05 02:12:50 +0530

    14
    add map feature ke liye dependencies

--- sos_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sos_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import location
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }

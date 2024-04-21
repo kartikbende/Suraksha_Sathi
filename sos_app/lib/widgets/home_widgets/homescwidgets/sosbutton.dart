67bce7e643c8f206aed6450b7afb990355d4a107
kartikbende <80601764+kartikbende@users.noreply.github.com>
2024-04-22 05:25:56 +0530

    53
    ss

--- sos_app/lib/widgets/home_widgets/homescwidgets/sosbutton.dart
+++ sos_app/lib/widgets/home_widgets/homescwidgets/sosbutton.dart
@@ -1,7 +1,44 @@
+import 'package:background_sms/background_sms.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class SOSbtn extends StatelessWidget {
+class SOSbtn extends StatefulWidget {
   const SOSbtn({super.key});
+
+  @override
+  State<SOSbtn> createState() => _SOSbtnState();
+}
+
+class _SOSbtnState extends State<SOSbtn> {
+  _getPermission() async => await [Permission.sms].request();
+  _isPermissionGranted() async => await Permission.sms.status.isGranted;
+  _sendSms(String phoneNumber, String message, {int? simSlot}) async {
+    await BackgroundSms.sendMessage(
+            phoneNumber: phoneNumber, message: message, simSlot: simSlot)
+        .then(
+      (SmsStatus status) {
+        if (status == "sent") {
+          Fluttertoast.showToast(msg: "Sent");
+        } else {
+          Fluttertoast.showToast(msg: "Failed");
+        }
+      },
+    );
+  }
+
+  showmodelforsos(
+    BuildContext context,
+  ) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+            height: MediaQuery.of(context).size.height / 1.4,
+            color: Colors.amber);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +55,9 @@ class SOSbtn extends StatelessWidget {
               // onPressed: () {},
 
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  showmodelforsos(context);
+                },
                 child: Image.asset(
                   'assests/sosbtn.png',
                 ),
